ddd0826a0b0dcd53ecbee9e17b17c9fb562e89e1
Ayman Naeem <Aymannk666@gmail.com>
2026-09-13 15:30:42 +0300

    first commit

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
     venue: "كافيه ايليت بلازا",
     address: " دمشق - الميدان - ساحة الأشمر - خلف كازية المهايني ",
     mapsUrl: "https://maps.app.goo.gl/wzRsoxYNZk2EFQvQA",
-    whatsapp: "0949330851"
+    whatsapp: "00963949330851"
   };
 
   // Each caption is paired with the photo that has the same number.
